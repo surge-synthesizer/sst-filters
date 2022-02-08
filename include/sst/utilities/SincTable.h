e8d6ff64013a216dbc36e14a91cf86af10234d30
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2022-02-08 23:06:57 +0000

    Implement comb filter types (#10)
    * Implemented comb filters, but now something is broken?
    
    * Silly hack: running BasicFiltersTest last helps CutoffWarpTest not fail?

---
+++ include/sst/utilities/SincTable.h
@@ -0,0 +1,80 @@
+#ifndef SST_FILTERS_SINCTABLE_H
+#define SST_FILTERS_SINCTABLE_H
+
+#include <cmath>
+
+struct SincTable
+{
+    static constexpr int FIRipol_M = 256;
+    static constexpr int FIRipol_M_bits = 8;
+    static constexpr int FIRipol_N = 12;
+    static constexpr int FIRoffset = FIRipol_N >> 1;
+    static constexpr int FIRipolI16_N = 8;
+    static constexpr int FIRoffsetI16 = FIRipolI16_N >> 1;
+
+    float sinctable alignas(16)[(FIRipol_M + 1) * FIRipol_N * 2];
+    float sinctable1X alignas(16)[(FIRipol_M + 1) * FIRipol_N];
+    short sinctableI16 alignas(16)[(FIRipol_M + 1) * FIRipolI16_N];
+
+    static inline double sincf(double x)
+    {
+        if (x == 0)
+            return 1;
+        return (sin(M_PI * x)) / (M_PI * x);
+    }
+
+    static inline double symmetric_blackman(double i, int n)
+    {
+        // if (i>=n) return 0;
+        i -= (n / 2);
+        return (0.42 - 0.5 * cos(2 * M_PI * i / (n)) + 0.08 * cos(4 * M_PI * i / (n)));
+    }
+
+    SincTable()
+    {
+        float cutoff = 0.455f;
+        float cutoff1X = 0.85f;
+        float cutoffI16 = 1.0f;
+        int j;
+        for (j = 0; j < FIRipol_M + 1; j++)
+        {
+            for (int i = 0; i < FIRipol_N; i++)
+            {
+                double t = -double(i) + double(FIRipol_N / 2.0) + double(j) / double(FIRipol_M) - 1.0;
+                double val = (float)(symmetric_blackman(t, FIRipol_N) * cutoff * sincf(cutoff * t));
+                double val1X =
+                    (float)(symmetric_blackman(t, FIRipol_N) * cutoff1X * sincf(cutoff1X * t));
+                sinctable[j * FIRipol_N * 2 + i] = (float)val;
+                sinctable1X[j * FIRipol_N + i] = (float)val1X;
+            }
+        }
+        for (j = 0; j < FIRipol_M; j++)
+        {
+            for (int i = 0; i < FIRipol_N; i++)
+            {
+                sinctable[j * FIRipol_N * 2 + FIRipol_N + i] =
+                    (float)((sinctable[(j + 1) * FIRipol_N * 2 + i] -
+                             sinctable[j * FIRipol_N * 2 + i]) /
+                            65536.0);
+            }
+        }
+
+        for (j = 0; j < FIRipol_M + 1; j++)
+        {
+            for (int i = 0; i < FIRipolI16_N; i++)
+            {
+                double t =
+                    -double(i) + double(FIRipolI16_N / 2.0) + double(j) / double(FIRipol_M) - 1.0;
+                double val =
+                    (float)(symmetric_blackman(t, FIRipolI16_N) * cutoffI16 * sincf(cutoffI16 * t));
+
+                sinctableI16[j * FIRipolI16_N + i] = (short)((float)val * 16384.f);
+            }
+        }
+    }
+};
+
+// TODO: is this safe??
+const static SincTable globalSincTable;
+
+#endif // SST_FILTERS_SINCTABLE_H
--- include/sst/utilities/globals.h
+++ include/sst/utilities/globals.h
@@ -36,25 +36,7 @@
 static inline int _stricmp(const char *s1, const char *s2) { return strcasecmp(s1, s2); }
 #endif
 
-//#define _SURGE_STR(x) #x
-//#define SURGE_STR(x) _SURGE_STR(x)
-//
-// const int BASE_WINDOW_SIZE_X = 913;
-// const int BASE_WINDOW_SIZE_Y = 569;
-// const int NAMECHARS = 64;
-// const int BLOCK_SIZE = 32;
-// const int OSC_OVERSAMPLING = 2;
-// const int BLOCK_SIZE_OS = OSC_OVERSAMPLING * BLOCK_SIZE;
-// const int BLOCK_SIZE_QUAD = BLOCK_SIZE >> 2;
-// const int BLOCK_SIZE_OS_QUAD = BLOCK_SIZE_OS >> 2;
-// const int OB_LENGTH = BLOCK_SIZE_OS << 1;
-// const int OB_LENGTH_QUAD = OB_LENGTH >> 2;
-// const float BLOCK_SIZE_INV = (1.f / BLOCK_SIZE);
-// const float BLOCK_SIZE_OS_INV = (1.f / BLOCK_SIZE_OS);
-// const int MAX_FB_COMB = 2048; // must be 2^n
-// const int MAX_VOICES = 64;
-// const int MAX_UNISON = 16;
-// const int N_OUTPUTS = 2;
-// const int N_INPUTS = 2;
+constexpr int MAX_FB_COMB = 2048; // must be 2^n
+constexpr int MAX_FB_COMB_EXTENDED = 2048 * 64;
 
 #endif // SST_FILTERS_GLOBALS_H
