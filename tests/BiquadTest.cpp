db6dc3cb74a34a62315141cddb6d46495fd14ecd
Paul Walker <paul@pwjw.com>
2024-05-27 20:30:40 -0400

    Move the Biquad Filter to our concepts mechanism
    1. Express the constraints for the adapter as concepts
    2. Use the common getter to normalize the names into something
       more rational and less direct

--- tests/BiquadTest.cpp
+++ tests/BiquadTest.cpp
@@ -4,10 +4,12 @@
 
 struct TP
 {
-
+    float noteToPitch(float f) { return 0; }
+    float dbToLinear(float f) { return 1.f; }
+    float getSampleRateInv() { return 1.0 / 48000; }
 };
 
-TEST_CASE( "Simple Biquad" )
+TEST_CASE("Simple Biquad")
 {
     using bqt = sst::filters::Biquad::BiquadFilter<TP, 32>;
 
@@ -16,14 +18,14 @@ TEST_CASE( "Simple Biquad" )
 
     bq.coeff_LP(0.6, 0.01);
 
-    float x alignas(16) [32];
-    float y alignas(16) [32];
+    float x alignas(16)[32];
+    float y alignas(16)[32];
     // a square wave through an LP will get attenuated
     float phase = 0;
     float dphase = 1.0 / 79.4;
-    for (int i=0; i<50; ++i)
+    for (int i = 0; i < 50; ++i)
     {
-        for (int j=0; j<32; ++j)
+        for (int j = 0; j < 32; ++j)
         {
             x[j] = phase > 0.5 ? 1 : -1;
             phase += dphase;
@@ -31,7 +33,7 @@ TEST_CASE( "Simple Biquad" )
                 phase -= 1;
         }
         bq.process_block_to(x, y);
-        for (int j=0; j<32; ++j)
+        for (int j = 0; j < 32; ++j)
         {
             // bounded
             REQUIRE(abs(x[j]) >= abs(y[j]));
